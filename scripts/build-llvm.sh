f452d055f1fcd252d23586710a313585644dc99a
Ruizhe Zhao <vincentzhaorz@gmail.com>
2021-10-08 11:44:53 +0100

    Removed old llvm and added polygeist

--- scripts/build-llvm.sh
+++ scripts/build-llvm.sh
@@ -9,8 +9,6 @@ echo ""
 echo ">>> Install LLVM for Phism"
 echo ""
 
-TARGET="${1:-"local"}"
-
 # If ninja is available, use it.
 CMAKE_GENERATOR="Unix Makefiles"
 if which ninja &>/dev/null; then
@@ -20,16 +18,12 @@ fi
 # The absolute path to the directory of this script.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-if [ "${TARGET}" == "local" ]; then
-  "${DIR}/check-vitis.sh" || { echo "Xilinx Vitis check failed."; exit 1; }
-fi
-
 # Make sure llvm submodule is up-to-date.
 git submodule sync
 git submodule update --init --recursive
 
 # Go to the llvm directory and carry out installation.
-LLVM_DIR="${DIR}/../llvm"
+LLVM_DIR="${DIR}/../polygeist/llvm-project"
 
 cd "${LLVM_DIR}"
 mkdir -p build
@@ -37,11 +31,9 @@ cd build
 
 # Configure CMake
 if [ ! -f "CMakeCache.txt" ]; then
-  export CC=gcc
-  export CXX=g++ 
   cmake ../llvm \
-    -DLLVM_ENABLE_PROJECTS="mlir;llvm;clang" \
-    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DLLVM_ENABLE_PROJECTS="mlir;clang" \
+    -DCMAKE_BUILD_TYPE=DEBUG \
     -DLLVM_BUILD_EXAMPLES=OFF \
     -DLLVM_TARGETS_TO_BUILD="host" \
     -DLLVM_OPTIMIZED_TABLEGEN=ON \
@@ -49,14 +41,16 @@ if [ ! -f "CMakeCache.txt" ]; then
     -DLLVM_ENABLE_BINDINGS=OFF \
     -DLLVM_INSTALL_UTILS=ON \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DBUILD_POLYMER=ON \
-    -DPLUTO_LIBCLANG_PREFIX="$(llvm-config --prefix)" \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DLLVM_USE_LINKER=lld \
     -G "${CMAKE_GENERATOR}"
 fi 
  
 # Run building
 if [ "${CMAKE_GENERATOR}" == "Ninja" ]; then
   ninja
+  ninja check-mlir
 else 
   make -j "$(nproc)"
 fi
